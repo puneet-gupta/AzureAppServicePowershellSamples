6c2b5eeb612b979d331cd02c3c8030bc994d008c
puneet-gupta <puneetg@outlook.com>
2016-03-22 20:54:51 +0530

    Adding Script to Append AppSettings

---
+++ AppendAppSettingToExistingCollection.ps1
@@ -0,0 +1,24 @@
+﻿. .\Functions.ps1
+
+$webAppName = "powershelldemowebapp"
+$subscriptionId = "31af7401-5a70-49be-80c7-7a6b85da7287"
+
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+     
+$rgName = GetResourceGroupForWebApp $webAppName
+$resource = Invoke-AzureRmResourceAction -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName $webAppName/appsettings -Action list -ApiVersion 2015-08-01 -Force
+$appSettings = $resource.Properties
+
+$script:NewAppSettingHashTable = @{}
+$appSettings.psobject.properties | Foreach { 
+    $script:NewAppSettingHashTable.Add($_.Name,$_.Value) 
+}
+
+#Add APPSETTING with the name MyAppSettingName=MyAppSettingValue
+      
+$script:NewAppSettingHashTable.Add("MyNewAppSettingName", "MyNewAppSettingValue")
+
+New-AzureRmResource -PropertyObject $script:NewAppSettingHashTable -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Web/sites/config -ResourceName $webAppName/appsettings -ApiVersion 2015-08-01 -Force
+
+

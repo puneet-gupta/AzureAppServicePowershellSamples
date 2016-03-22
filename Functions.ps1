6101ff03beea480d5e5dd77156b84a178754c018
puneet-gupta <puneetg@outlook.com>
2016-03-22 12:44:38 +0530

    first commit

---
+++ Functions.ps1
@@ -0,0 +1,11 @@
+﻿Function GetResourceGroupForWebApp ([string] $webappNameParam)
+{
+
+    $ResourceGroupName = ""
+    Get-AzureRmResource | where { ($_.ResourceType -match "Microsoft.Web/sites") -and ($_.ResourceId.ToLower().EndsWith($webappNameParam.ToLower()))} | foreach     
+    {        
+       $ResourceGroupName = $_.ResourceGroupName
+        break
+    }
+    return $ResourceGroupName
+}

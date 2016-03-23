99d2ce2a8bb9d55bc857ff1cf90e65bb25315d36
puneet-gupta <puneetg@outlook.com>
2016-03-23 12:31:58 +0530

    Adding Script for swapping slots

--- SwapAzureWebAppWithSlot.ps1
+++ SwapAzureWebAppWithSlot.ps1
@@ -1,1 +1,19 @@
 ﻿
+
+$webAppName = "powershelldemowebapp"
+$subscriptionId = "31af7401-5a70-49be-80c7-7a6b85da7287"
+
+#Login-AzureRmAccount
+#Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+
+$rgName = GetResourceGroupForWebApp $webAppName
+
+$ParametersObject = @{
+	targetSlot = "staging"
+	preserveVnet = "true"
+}
+
+Invoke-AzureRmResourceAction -ResourceGroupName $rgName `
+    -ResourceType Microsoft.Web/sites `
+    -ResourceName $webAppName -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force

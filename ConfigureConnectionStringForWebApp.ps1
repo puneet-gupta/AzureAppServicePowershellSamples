09f510ec92c1fad4c885ed69c4611cebddfe77cc
puneet-gupta <puneetg@outlook.com>
2016-03-23 12:17:32 +0530

    Changing script to append Connection Strings

--- ConfigureConnectionStringForWebApp.ps1
+++ ConfigureConnectionStringForWebApp.ps1
@@ -1,4 +1,4 @@
-﻿. .\Functions.ps1 # This script contains the GetResourceGroupForWebApp Function and code of this script is @ https://github.com/puneet-gupta/AzureAppServicePowershellSamples/blob/master/Functions.ps1
+﻿. .\Functions.ps1
 
 $webAppName = "powershelldemowebapp"
 $subscriptionId = "31af7401-5a70-49be-80c7-7a6b85da7287"
@@ -6,13 +6,29 @@ $subscriptionId = "31af7401-5a70-49be-80c7-7a6b85da7287"
 Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionId $subscriptionId
 
-$rgName = GetResourceGroupForWebApp $webAppName 
-	 
+
+$rgName = GetResourceGroupForWebApp $webAppName
+$resource = Invoke-AzureRmResourceAction -ResourceGroupName $rgName `
+    -ResourceType Microsoft.Web/sites/config `
+    -ResourceName $webAppName/connectionstrings `
+    -Action list -ApiVersion 2015-08-01 -Force
+
+$connectionStrings = $resource.Properties
+
+$script:NewConnectionStringsTables = @{}
+
+$connectionStrings.psobject.properties | Foreach { 
+    $script:NewConnectionStringsTables.Add($_.Name,$_.Value) 
+}
+
+#Add ConnectiongString with the name ConnstringKey	 
 #Type=2 is SQL Database (Which means SQL Azure)
 #Type=1 is a SQL Server.
-$PropertiesObject = @{ ConnstringKey = @{ value = "The Actual connecting string here" ; Type = 2 };}
+      
+$script:NewConnectionStringsTables.Add("ConnstringKey", @{ value = "The Actual connecting string here" ; Type = 2 })
+
 	    
-New-AzureRmResource -PropertyObject $PropertiesObject `
+New-AzureRmResource -PropertyObject $script:NewConnectionStringsTables `
 -ResourceGroupName $rgName `
 -ResourceType Microsoft.Web/sites/config `
 -ResourceName $webAppName/connectionstrings `

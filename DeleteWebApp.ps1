ee3791d43b736749d9827fe8e46ef1459d9a24fd
puneet-gupta <puneetg@outlook.com>
2016-03-23 12:40:26 +0530

    Adding more scripts

---
+++ DeleteWebApp.ps1
@@ -0,0 +1,12 @@
+﻿$webAppName = "powershelldemowebapp"
+$subscriptionId = "31af7401-5a70-49be-80c7-7a6b85da7287"
+
+#Login-AzureRmAccount
+#Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+
+$rgName = GetResourceGroupForWebApp $webAppName
+
+Remove-AzureRmResource -ResourceGroupName $rgName -ResourceType Microsoft.Web/sites `
+    -ResourceName $webAppName `
+    -ApiVersion 2015-08-01 -Force
---
+++ DownloadPublishProfileForWebApp.ps1
@@ -0,0 +1,20 @@
+﻿
+$webAppName = "powershelldemowebapp"
+$subscriptionId = "31af7401-5a70-49be-80c7-7a6b85da7287"
+
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+
+$rgName = GetResourceGroupForWebApp $webAppName
+
+
+$publishProfileString = Invoke-AzureRmResourceAction -ResourceGroupName $rgName `
+     -ResourceType Microsoft.Web/sites `
+     -ResourceName $webAppName `
+     -Action publishxml `
+     -ApiVersion 2015-08-01 -Force
+
+$publishProfileString | Out-File -FilePath c:\temp\publishprofile.xml
+
+notepad.exe c:\temp\publishprofile.xml

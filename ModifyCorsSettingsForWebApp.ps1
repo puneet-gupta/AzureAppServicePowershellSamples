f36a4e89cf95917ad1e7da42802df6134de1aa85
Puneet Gupta <puneetg@outlook.com>
2016-06-10 17:11:01 +0530

    Update ModifyCorsSettingsForWebApp.ps1

--- ModifyCorsSettingsForWebApp.ps1
+++ ModifyCorsSettingsForWebApp.ps1
@@ -1,15 +1,17 @@
-Login-AzureRmAccount
+. .\Functions.ps1 # This script contains the GetResourceGroupForWebApp Function and code of this script is @ https://github.com/puneet-gupta/AzureAppServicePowershellSamples/blob/master/Functions.ps1
 
-$siteName = "buggybits"
-$rgGroup = "buggybits"
+$webAppName = "powershelldemowebapp"
+$subscriptionId = "31af7401-5a70-49be-80c7-7a6b85da7287"
+
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+$rgName = GetResourceGroupForWebApp $webAppName
 
 $allowedOrigins = @()
-
 $allowedOrigins += "*"
 $allowedOrigins += "www.yourdomain.com"
 
-$allowedOrigins
-
 $PropertiesObject = @{cors = @{allowedOrigins= $allowedOrigins}}
 
-Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName $rgGroup -ResourceType Microsoft.Web/sites/config -ResourceName $siteName/web -ApiVersion 2015-08-01 -Force
+Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName $rgName -ResourceType Microsoft.Web/sites/config -ResourceName $webAppName/web -ApiVersion 2015-08-01 -Force
